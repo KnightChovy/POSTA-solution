3228af899ca942ac2322bd1c0bb4dd05099a017e
khiembadinh987@gmail.com <khiembadinh987@gmail.com>
2026-05-30 01:31:27 +0700

    fix bug lan 1

--- admin-backend/src/app/controllers/imageController.js
+++ admin-backend/src/app/controllers/imageController.js
@@ -3,19 +3,16 @@ const Post = require("../models/Post")
 const saveImageToServer = async (req, res) => {
   const files = req.files || (req.file ? [req.file] : []);
   console.log("Received files: ", files)
-  if (!files || files.length === 0) {
-    return null;
-  }
 
+  // Luôn tạo Post (imagePath rỗng nếu không upload ảnh) để không trả về null,
+  // tránh các bước sau dereference newPost._id trên null gây crash.
   const imagePath = files.map(file =>
     file.path
       .replace(/\\/g, "/")
       .replace(/^src\/?/, "")
   );
 
-  const newPost = new Post({});
-  newPost.imagePath = imagePath;
-
+  const newPost = new Post({ imagePath });
   await newPost.save();
   return newPost._id;
 }
